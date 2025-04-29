02009f7b737adf9a62fe65be0dfa9daa18c54ef7
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2025-04-29 11:46:10 -0400

    generating reports by vendor, year to date, prev year, prev month, month to date

---
+++ AccountingLedger/src/main/java/com/pluralsight/model/Report.java
@@ -0,0 +1,51 @@
+package com.pluralsight.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Report {
+    private float balance = 0;
+    private float totalPayments = 0;
+    private float totalDeposits = 0;
+    private ArrayList<LedgerEntry> deposits = new ArrayList<>();
+    private ArrayList<LedgerEntry> payments = new ArrayList<>();
+    private TreeMap<LocalDateTime, LedgerEntry> entries;
+
+    public Report(TreeMap<LocalDateTime, LedgerEntry> entries) {
+        this.entries = entries;
+    }
+
+    public void displayTransactions() {
+        System.out.println("TRANSACTIONS:");
+        for (LedgerEntry entry : this.entries.values()) {
+            balance += entry.getAmount();
+            System.out.println(entry);
+
+            if (entry.getAmount() <= 0) {
+                payments.add(entry);
+            } else {
+                deposits.add(entry);
+            }
+        }
+        System.out.printf("Balance: %.2f\n", balance);
+    }
+
+    public void displayDeposits() {
+        System.out.println("\nDEPOSITS:");
+        for (LedgerEntry deposit : deposits) {
+            System.out.println(deposit);
+            totalDeposits += deposit.getAmount();
+        }
+        System.out.printf("Total Deposits: %.2f\n", totalDeposits);
+    }
+
+    public void displayPayments() {
+        System.out.println("\nPAYMENTS:");
+        for (LedgerEntry payment : payments) {
+            System.out.println(payment);
+            totalPayments += payment.getAmount();
+        }
+        System.out.printf("Total Payments: %.2f\n", totalPayments);
+    }
+}
--- AccountingLedger/src/main/java/com/pluralsight/view/Report.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.view;
-
-public class Report {
-    public static void returnToReports(){
-        System.out.println("Type 0 to return to Reports or 1 to generate a CSV");
-
-    }
-}
